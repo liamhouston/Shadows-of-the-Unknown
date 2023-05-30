f908bcf54e9fd9775f54c6701bdbf3b0873ce705
Emily Halina <ehalina@ualberta.ca>
2023-05-30 16:21:56 -0600

    initial 2DLevelDesign commit with base scene: from after this commit, will edit in my own individual copy of the scene!

---
+++ Assets/Labs/2D Level Design/Base/Scripts/TopDownEnemyBehaviour.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownEnemyBehaviour : TopDownEntityBehaviour
+{
+
+    // public variables
+    public float moveTime = 1.5f;
+    public float damageRadius = 2f;
+
+    // internal variables
+    private float _remainingTime;
+
+    // the player
+    private Rigidbody2D player;
+    
+
+    // Start is called before the first frame update
+    override public void Start()
+    {
+        base.Start();
+        _remainingTime = moveTime;
+        _currDir = pickDirection();
+        player = (Rigidbody2D)GameObject.Find("Player").GetComponent("Rigidbody2D");
+    }
+    
+    // Update is called once per frame
+    override public void FixedUpdate()
+    {
+        base.FixedUpdate();
+
+        // handle movement, change directions after elapsed time
+        _remainingTime -= Time.deltaTime;
+        if (_remainingTime <= 0){
+            _currDir = pickDirection();
+            _remainingTime = moveTime;
+        }
+
+        // handle player collision
+        if (Mathf.Abs(Vector2.Distance(player.position, transform.position)) <= damageRadius){
+            player.gameObject.SendMessage("takeDamage");
+        }
+    }
+
+    // uses helper function to convert current direction into movement vector
+    override public Vector2 getMovement(){
+        return dirToVec();
+    }
+
+    // update our direction with a random new one
+    Direction pickDirection(){
+        int i = 0; // loop limiter for the uh. 0.00390625 chance 
+        
+        // pick a new direction that wasn't our old one
+        Direction newDir = (Direction)Random.Range(0, 4);
+        while (newDir == _currDir && i < 100){
+            newDir = (Direction)Random.Range(0, 4);
+            i++;
+        }
+        return newDir;
+    }
+    
+    // on death, kill!!
+    override public void handleDeath(){
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Labs/2D Level Design/Base/Scripts/TopDownPlayerBehaviour.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class TopDownPlayerBehaviour : TopDownEntityBehaviour
+{
+    [Header("Input Bindings")]
+    [SerializeField] public KeyCode left = KeyCode.LeftArrow;
+    [SerializeField] public KeyCode right = KeyCode.RightArrow;
+    [SerializeField] public KeyCode up = KeyCode.UpArrow;
+    [SerializeField] public KeyCode down = KeyCode.DownArrow;
+    [SerializeField] public KeyCode attack = KeyCode.Space;
+    [SerializeField] public KeyCode reset = KeyCode.R;
+
+    // public variables
+    public float attackCooldown = 2f;
+
+    // internal variables    
+    // attack parameters
+    private bool _canAttack = true;
+    private float _attackThreshold = 1.5f;
+    private float _attackCountdown;
+    
+    // Start is called before the first frame update
+    override public void Start()
+    {
+        base.Start();
+        _attackCountdown = attackCooldown;
+    }
+
+    // Update is called once per frame
+    override public void FixedUpdate()
+    {
+        base.FixedUpdate();
+
+        // handle attack cooldown
+        if (!_canAttack){
+            _attackCountdown -= Time.deltaTime;
+            if (_attackCountdown <= 0f){
+                _canAttack = true;
+                _attackCountdown = attackCooldown;
+            }
+        }
+
+        // handle attack and damage collisions
+        if (Input.GetKey(attack)){
+            handleAttack();
+        }
+
+        // reset button
+        if (Input.GetKey(reset)){
+            handleDeath();
+        }
+
+        
+    }
+
+    override public Vector2 getMovement(){
+        Vector2 update = Vector2.zero;
+        // read input, and set our direction accordingly
+        if (Input.GetKey(right)){
+            update.x = 1;
+            _currDir = Direction.East;
+        }
+        else if (Input.GetKey(left)){
+            update.x = -1;
+            _currDir = Direction.West;
+        }
+        else if (Input.GetKey(up)){
+            update.y = 1;
+            _currDir = Direction.North;
+        }
+        else if (Input.GetKey(down)){
+            update.y = -1;
+            _currDir = Direction.South;
+        }
+        return update;
+    }
+
+    void handleAttack(){
+        // if we can't attack, don't
+        if (!_canAttack) { return; }
+
+        // shoot out a ray looking to ATTACK
+        _canAttack = false;
+        Vector2 attackDir = dirToVec();
+
+        RaycastHit2D attackRay = Physics2D.Raycast(rb.position, attackDir);
+        if (attackRay.collider && attackRay.distance < _attackThreshold){
+            Debug.Log("hit!");
+            Debug.Log(_currDir);
+
+            attackRay.collider.gameObject.SendMessage("takeDamage");
+
+        }
+        else{
+            Debug.Log("no hit!");
+        }
+    }
+
+    override public void handleDeath(){
+        // todo: death anim / sound, wait on anim/sound
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
