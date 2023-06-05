7e4e2fbba3790f4c61b5a3af02c46efc31d98c17
Megan Sumner <msumner@ualberta.ca>
2023-06-05 14:21:02 -0600

    Adding general sokoban script that can either go on the player or level to manage the number of crates and goals.

--- Assets/Labs/2D Level Design/Base/Scripts/TopDownCrateBehaviour.cs
+++ Assets/Labs/2D Level Design/Base/Scripts/TopDownCrateBehaviour.cs
@@ -4,43 +4,29 @@ using UnityEngine;
 
 public class TopDownCrateBehaviour : MonoBehaviour
 {
-    // the player
+
+    private TopDownSokobanBehaviour sokobanScript;
     private Rigidbody2D player;
-    private TopDownPlayerBehaviour playerScript;
 
     // Start is called before the first frame update
     void Start()
     {
         player = (Rigidbody2D)GameObject.Find("Player").GetComponent("Rigidbody2D");
-        playerScript = (TopDownPlayerBehaviour)player.gameObject.GetComponent(typeof(TopDownPlayerBehaviour));
-
-        VerifyCrates();
+        sokobanScript = (TopDownSokobanBehaviour)player.gameObject.GetComponent(typeof(TopDownSokobanBehaviour));
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     //Just overlapped a collider 2D
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Crate")
+        if (collision.gameObject.tag == "Goal")
         {
+            sokobanScript.DecrementGoals();
+            sokobanScript.DecrementCrates();
+
             Destroy(collision.gameObject);
             Destroy(gameObject);
         }
     }
 
-    void VerifyCrates()
-    {
-        GameObject[] crates = GameObject.FindGameObjectsWithTag("Crate");
-        GameObject[] goals = GameObject.FindGameObjectsWithTag("Goal");
-
-        if (crates.Length != goals.Length)
-        {
-            Debug.LogError(("Number of crates and goals for Sokoban are not equal. \nNumber of Crates: " + crates.Length + " Number of Goals: " + goals.Length));
-        }
-    }
 }
---
+++ Assets/Labs/2D Level Design/Base/Scripts/TopDownSokobanBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownSokobanBehaviour : MonoBehaviour
+{
+    // Internal Parameters
+    private int _crates;
+    private int _goals;
+
+    // Check if Sokoban puzzle complete
+    public bool puzzleComplete;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        puzzleComplete = false;
+
+        VerifyCrates();
+    }
+
+    void VerifyCrates()
+    {
+        _goals = GameObject.FindGameObjectsWithTag("Goal").Length;
+        _crates = GameObject.FindGameObjectsWithTag("Crate").Length;
+
+        if (_crates != _goals)
+        {
+            Debug.LogError(("Number of crates and goals for the puzzle are not equal. \nNumber of Crates: " + _crates + " Number of Goals: " + _goals));
+        }
+    }
+
+    public void DecrementGoals()
+    {
+        _goals--;
+
+        if (_goals <= 0)
+        {
+            Debug.Log("Puzzle complete!");
+            puzzleComplete = true;
+        }
+    }
+
+    public void DecrementCrates()
+    {
+        _crates--;
+    }
+}
