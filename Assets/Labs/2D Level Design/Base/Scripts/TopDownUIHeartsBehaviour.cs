020c5053e42296aac2f894d81a00ae560cd01f6f
Emily Halina <59850545+emily-halina@users.noreply.github.com>
2023-06-07 20:05:08 -0600

    2DLevelDesign: Projectile Enemy & UI (#13)
    * projectile enemy!
    
    * Added UI + TextMeshPro

---
+++ Assets/Labs/2D Level Design/Base/Scripts/TopDownUIHeartsBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownUIHeartsBehaviour : MonoBehaviour
+{
+
+    // the player
+    private Rigidbody2D player;
+    private TopDownPlayerBehaviour playerScript;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = (Rigidbody2D)GameObject.Find("Player").GetComponent("Rigidbody2D");
+        playerScript = (TopDownPlayerBehaviour)player.gameObject.GetComponent(typeof(TopDownPlayerBehaviour));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int currHealth = playerScript.getHealth();
+        for (int i = 0; i < transform.childCount; i++){
+            if (i > currHealth - 1){
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+}
