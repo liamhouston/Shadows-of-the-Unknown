373d0c2aa8f04fffbc3758946cb2419dc5ca2192
Megan Sumner <msumner@ualberta.ca>
2023-07-09 23:51:48 -0600

    Fixing crate movement on ice. Revamped the crates to use the entity collision and movement system that players and enemies were using.

--- Assets/Labs/2D Level Design/Base/Scripts/TopDownCrateBehaviour.cs
+++ Assets/Labs/2D Level Design/Base/Scripts/TopDownCrateBehaviour.cs
@@ -2,60 +2,78 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TopDownCrateBehaviour : MonoBehaviour
+public class TopDownCrateBehaviour : TopDownEntityBehaviour
 {
 
     private TopDownSokobanBehaviour sokobanScript;
+    private TopDownPlayerBehaviour playerScript;
     private Rigidbody2D player;
-    private Rigidbody2D rb;
+    private Rigidbody2D crate;
 
     // ice parameters
     private Vector2 _closestIce = Vector2.positiveInfinity;
     private bool _isOnIce = false;
     private float _iceThreshold = 0.9f;
     private bool _lockForce = false;
-    private Vector2 latestVelocity;
+    private Vector2 previousLocation;
+    private Vector2 lastPlayerLocation;
 
     // Start is called before the first frame update
-    void Start()
+    override public void Start()
     {
+        base.Start();
+
         player = (Rigidbody2D)GameObject.Find("Player").GetComponent("Rigidbody2D");
-        rb = gameObject.GetComponent<Rigidbody2D>();
+        playerScript = (TopDownPlayerBehaviour)player.gameObject.GetComponent(typeof(TopDownPlayerBehaviour));
+
+        crate = gameObject.GetComponent<Rigidbody2D>();
         sokobanScript = (TopDownSokobanBehaviour)player.gameObject.GetComponent(typeof(TopDownSokobanBehaviour));
+        Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
     }
 
     // Update is called once per frame
-    public void FixedUpdate()
+    override public void FixedUpdate()
     {
+        base.FixedUpdate();
+
         updateIce();
+        previousLocation = crate.position;
+    }
 
-        // Check if we are on ice and get latest velocity before being on Ice
-        if (_isOnIce && !isStopped()) {
-            if (!_lockForce )
-            {
-                latestVelocity = rb.velocity;
-                _lockForce = true;
-            }
-
-            // Check if we are going faster in the x direction or y direction and move on ice with highest direction
-            // This should prevent diagonal ice movement
-            if (Mathf.Abs(latestVelocity.x) >= Mathf.Abs(latestVelocity.y) && _lockForce)
-            {
-                rb.velocity = new Vector2(Mathf.Sign(latestVelocity.x) * 10, 0);
-            }
-            else if (Mathf.Abs(latestVelocity.x) < Mathf.Abs(latestVelocity.y) && _lockForce)
-            {
-                rb.velocity = new Vector2(0, Mathf.Sign(latestVelocity.y) * 10);
-            }
-        }
-
-        if (!_isOnIce)
+    override public Vector2 getMovement()
+    {
+        Vector2 update = Vector2.zero;
+       
+        if (!_lockForce)
         {
-            if (_lockForce)
+            lastPlayerLocation = playerScript.dirToVec();
+        }    
+        // if we are on ice, we're moving in our current direction
+        if (_isOnIce)
+        {
+            _lockForce = true;
+            return lastPlayerLocation;
+        }
+
+        else if (!_isOnIce || isStopped())
+        {
+            _lockForce = false;
+        }
+
+        // handle player collision with crate
+        if (Mathf.Abs(Vector2.Distance(player.position, transform.position)) <= 1f)
+        {
+            if (Mathf.Abs(Mathf.Abs(player.position.x) - Mathf.Abs(crate.position.x)) > Mathf.Abs(Mathf.Abs(player.position.y) - Mathf.Abs(crate.position.y)))
             {
-                _lockForce = false;
+                update = new Vector2(playerScript.dirToVec().x, 0);
+            }
+            else
+            {
+                update = new Vector2(0, playerScript.dirToVec().y);
             }
         }
+
+        return update;
     }
 
     //Just overlapped a collider 2D
@@ -84,19 +102,18 @@ public class TopDownCrateBehaviour : MonoBehaviour
             }
         }
 
-        if ((Mathf.Abs(_closestIce.x - transform.position.x) <= _iceThreshold && Mathf.Abs(_closestIce.y - transform.position.y) <= _iceThreshold) && !isStopped())
+        if (Mathf.Abs(_closestIce.x - transform.position.x) <= _iceThreshold && Mathf.Abs(_closestIce.y - transform.position.y) <= _iceThreshold && !isStopped())
         {
 
             _isOnIce = true;
         }
-        else
-        {
+        else{
             _isOnIce = false;
         }
     }
 
     public bool isStopped()
     {
-        return (rb.velocity.x == 0 && rb.velocity.y == 0);
+        return (crate.position.x == previousLocation.x && crate.position.y == previousLocation.y);
     }
 }
