ce3d1d17c3396c2621c268e6cc574c2ffcc93e1c
Emily Halina <59850545+emily-halina@users.noreply.github.com>
2023-08-02 18:05:46 -0600

    2D Level Design Lab Review Pt 2: PCG Lab & File Structure Stuff (#25)
    * Adding some sound effects, character animations, sprites, various fixes
    
    * minor merge fixes to prefab
    
    * bugfixes and sound pt2
    
    * initial lab commit
    
    * lab scaffolding
    
    * tweaks to lab componenets
    
    * fixes to sokoban system
    
    * more sokoban fixes
    
    * lab review fixes pt 1 (labs 1-4)
    
    * fixed small crate sound bug
    
    * removed print statements
    
    * PCG overhaul
    
    * renamed file structure and changed key sprite

---
+++ Assets/Labs/3 2D Level Design/3-5 PCG/Scripts/TopDownComponentHandler.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[ExecuteInEditMode]
+public class TopDownComponentHandler : MonoBehaviour
+{
+    GameObject[] components;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // space the components evenly in a grid
+        components = GameObject.FindGameObjectsWithTag("PCGComponent");
+        
+        int pos_x = 0;
+        int pos_y = 0;
+        int row = 0;
+
+        foreach (GameObject c in components){
+            c.transform.position = new Vector3(pos_x, pos_y, 0);
+            pos_x = (pos_x + 15) % 60;
+            if (row == 3){
+                pos_y -= 10;
+            }
+            row = (row + 1) % 4;
+        }
+    }
+
+    public void save(){
+        // get rid of any existing components
+        foreach (string f in Directory.EnumerateFiles("Assets/Labs/3 2D Level Design/3-5 PCG/Scenes/Components/")){
+            File.Delete(f);
+        }
+
+        components = GameObject.FindGameObjectsWithTag("PCGComponent");
+
+        for (int i = 0; i < components.Length; i++){
+            TopDownComponentScript t = components[i].GetComponent<TopDownComponentScript>();
+            t.saveToFile("component" + i.ToString() + ".txt");
+        }
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor (typeof(TopDownComponentHandler))]
+public class PCGEditor : Editor{
+    
+    public override void OnInspectorGUI (){
+        TopDownComponentHandler compHandler = (TopDownComponentHandler)target;
+        if (GUILayout.Button("Save Components")){
+            compHandler.save();
+        }
+        DrawDefaultInspector();
+    }
+}
+#endif
---
+++ Assets/Labs/3 2D Level Design/3-5 PCG/Scripts/TopDownComponentScript.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.Tilemaps;
+
+public class TopDownComponentScript : MonoBehaviour
+{
+    public bool topLeftOkay;
+    public bool topRightOkay;
+    public bool bottomLeftOkay;
+    public bool bottomRightOkay;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void saveToFile(string filename){
+        StreamWriter writer = new StreamWriter("Assets/Labs/3 2D Level Design/3-5 PCG/Scenes/Components/" + filename);
+
+        // first row, 4 bools denoting the 4 public variables
+        writer.WriteLine(topLeftOkay.ToString() + " " + topRightOkay.ToString() + " " + bottomLeftOkay.ToString() + " " + bottomRightOkay.ToString());
+        
+        // next, the 5x9 grid of tiles
+        Tilemap tiles = gameObject.GetComponentInChildren<Tilemap>();
+        
+        // 0, 0 is top left, and unity has up as positive, so negative
+        for (int i = 0; i > -5; i--){
+            string rowString = "";
+            for (int j = 0; j < 9; j++){
+                Vector3Int tilePosition = new Vector3Int(j, i, 0);
+                if (tiles.HasTile(tilePosition)){
+                    switch (tiles.GetTile(tilePosition).name){
+                        case "CaveRuleTile":
+                            rowString += "w";
+                            break;
+                        case "GroundTile":
+                            rowString += ".";
+                            break;
+                        case "IceRuleTile":
+                            rowString += "i";
+                            break;
+                        case "PitRuleTile":
+                            rowString += "p";
+                            break;
+                        default:
+                            rowString += "h";
+                            break;
+                    }
+                }
+            }
+            writer.WriteLine(rowString);
+        }
+
+        // n x 4 list of game objects in the scene
+        foreach (Transform child in transform){
+            string line = "";
+            switch (child.tag){
+                case "Enemy":
+                    // enemy 2
+                    if (child.gameObject.GetComponent<TopDownEnemy2Behaviour>() != null){
+                        line = "2 " + child.position.x + " " + child.position.y + " 0";
+                    }
+                    // enemy 1
+                    else{
+                        line = "1 " + child.position.x + " " + child.position.y + " 0";
+                    }
+                    break;
+    
+                case "Goal":
+                    line = "g " + child.position.x + " " + child.position.y + " 0";
+                    break;
+
+                case "Crate":
+                    line = "c " + child.position.x + " " + child.position.y + " 0";
+                    break;
+                
+                default:
+                    // key
+                    if (child.gameObject.GetComponent<TopDownKeyBehaviour>() != null){
+                        line = "k " + child.position.x + " " + child.position.y + " 0";
+                    }
+                    // door
+                    else if (child.gameObject.GetComponent<TopDownDoorBehaviour>() != null){
+                        int openCond = (int)child.gameObject.GetComponent<TopDownDoorBehaviour>().openCondition;
+                        line = "d " + child.position.x + " " + child.position.y + " " + openCond;
+                    }
+                    break;
+            }
+            if (line != ""){
+                writer.WriteLine(line);
+            }
+        }
+        
+        writer.Close();
+    }
+}
