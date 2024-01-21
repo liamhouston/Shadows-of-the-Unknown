15a2b20206600d9ba4382f742ad54e35f4edd476
Liam Houston <liamdh23@gmail.com>
2024-01-21 14:59:58 -0700

    added collision with bush to open new scene

---
+++ Assets/startCameraScene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startCameraScene : MonoBehaviour
+{
+    private bool playerIsNearby = false;
+    private string sceneToLoad = "CameraMiniGameScene";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (playerIsNearby && Input.GetKey(KeyCode.C)){
+            Debug.LogError("Opening a new Scene!");
+            //SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        Debug.LogError("Collision with bush");
+        if (other.CompareTag("Player")) playerIsNearby = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other){
+        Debug.LogError("Exit collision with bush");
+        if (other.CompareTag("Player")) playerIsNearby = false;
+    }
+}
