4c14143bef3ea76d6499db153ea3c9c7764bef48
Hyunsung Rho <hrho@ualberta.ca>
2024-03-06 12:41:47 -0700

    Fixed_path!

--- Assets/Scripts/BushesFishdock.cs
+++ Assets/Scripts/BushesFishdock.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class BushesFishdock : MonoBehaviour
 {
+    [SerializeField]
     private bool _isPic;
     // Update is called once per frame
     private void Start()
     {
-
-        _isPic = Player.Instance.TentPic;
-
+        if (GameController.Instance != null)
+        {
+            _isPic = Player.Instance.TentPic;
+        }
 
     }
     private void Update()
@@ -19,6 +22,11 @@ public class BushesFishdock : MonoBehaviour
         {
             Debug.Log("took the pic already");
             GetComponent<BoxCollider2D>().enabled = false;
+            TryGetComponent(out Light2D lightpath);
+            if (lightpath)
+            {
+                lightpath.enabled = true;
+            }
         }
     }
 }
