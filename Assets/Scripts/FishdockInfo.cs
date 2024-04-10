f20ff06d10758c45f338b573e4372225e97df9d8
Hyunsung Rho <hrho@ualberta.ca>
2024-04-10 15:01:35 -0600

    extra scene updated

--- Assets/Scripts/FishdockInfo.cs
+++ Assets/Scripts/FishdockInfo.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Fishdockinfo : MonoBehaviour
 {
     private const string _lastHorizontal = "LastHorizontal";
+    
+    [Header("Puzzle Objects")]
     public GameObject MotelPoster;
+    public GameObject PercyCam;
+    public GameObject Fishshop;
     private void Start()
     {
         TryGetComponent(out Animator _playerAnimator);
@@ -27,11 +31,28 @@ public class Fishdockinfo : MonoBehaviour
             {
                 this.transform.position = new Vector3(118.6f, -6.4f, 0f);
             }
+            else if (PlayerPrefs.GetString("FromScene") == "PercyCam")
+            {
+                _playerAnimator.SetFloat(_lastHorizontal, 1);
+                this.transform.position = new Vector3(-129f, -6.4f, 0f);
+            }
+            else if (PlayerPrefs.GetString("FromScene") == "Fishshop")
+            {
+                this.transform.position = new Vector3(-129f, -6.4f, 0f);
+            }
         }
 
         if (PlayerPrefs.GetInt("MotelPosterPuzzle") == 1)
         {
             MotelPoster.GetComponent<Collider2D>().enabled = false;
+        }
+        if (PlayerPrefs.GetInt("PercyCamPuzzle") == 1)
+        {
+            PercyCam.GetComponent<Collider2D>().enabled = false;
+        }
+        if (PlayerPrefs.GetInt("FishshopPuzzle") == 1)
+        {
+            Fishshop.GetComponent<Collider2D>().enabled = false;
         }
     }
 }
